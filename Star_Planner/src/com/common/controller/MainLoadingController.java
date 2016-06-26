169f45b9851af0c6fdd807f4dedb676b02a7ee15
sujin <sujin@192.168.10.5>
2016-06-26 22:44:57 +0900

    XE로 바꿔서 다시 올립니당

---
+++ Star_Planner/src/com/common/controller/MainLoadingController.java
@@ -0,0 +1,35 @@
+package com.common.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.common.service.MainLoadingServiceImpl;
+
+@Controller
+@RequestMapping("/main")
+public class MainLoadingController {
+	
+	@Autowired
+	MainLoadingServiceImpl mainService;
+	
+	@RequestMapping("/home")
+	@ResponseBody
+	public ModelAndView LoadingMain(){
+		
+		HashMap<String, Object> map = new HashMap<>();
+		
+		System.out.println("## MainLoadingController + LoadingMain");
+		List<String> list = mainService.getMainLink();
+		System.out.println(list);
+		map.put("list", list);
+
+		return new ModelAndView("/common/main.do", map);
+	}
+
+}
